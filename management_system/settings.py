dc1f9ade363fa38290692d195167fce3ac930c90
unknown <rattanaksak168@gmail.com>
2025-02-15 14:56:28 +0700

    Update database config

--- management_system/settings.py
+++ management_system/settings.py
@@ -98,7 +98,11 @@ DATABASES = {
     }
 }
 
+import dj_database_url
 
+DATABASES = {
+    'default': dj_database_url.config(conn_max_age=600)
+}
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
